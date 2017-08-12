619f6e3c8bfc844b42d020cf959a503b003efd7f
John Whisker <nguyen.dk0952@sinhvien.hoasen.edu.vn>
2017-08-12 13:43:16 +0700

    Update FFile

--- Assets/StackController.cs
+++ Assets/StackController.cs
@@ -3,50 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StackController : MonoBehaviour {
-	public int NumberofSlots;
-	public float SlotsHeight;
 
-	private List<PlanetController> planets;
-
-	void Start() {
-		planets = new List<PlanetController> ();
+	// Use this for initialization
+	void Start () {
+		
 	}
-
-
-	public float GetTopPos(){
-		return (planets.Count - 1) * SlotsHeight;
+	
+	// Update is called once per frame
+	void Update () {
+		
 	}
-
-	public bool AddPlanet(PlanetController planet){
-		if (planets.Count >= NumberofSlots)
-			return false;
-		planets.Add (planet);
-		return true;
-	}
-
-	public bool RemoveTop(){
-		if (planets.Count > 0) {
-			return planets.Remove(planets.FindLast(x=>x));
-		}
-		return false;
-	}
-
-	private PlanetController GetTopPlanet(){
-		return planets.FindLast (x => x);
-	}
-
-	public bool isEmpty(){
-		if (planets.Count > 0)
-			return false;
-		return true;
-	}
-
-	public int GetTopSize(){
-		return GetTopPlanet ().Size;
-	}
-
-	public float GetXPos() {
-		return this.transform.position.x;
-	}
-
 }
--- Assets/UFOController.cs
+++ Assets/UFOController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlanetController : MonoBehaviour {
+public class UFOController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
