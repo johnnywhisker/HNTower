886185bc0d27c92ffcc5659a7d99c9d70d5a376e
Vương Nguyễn <vuong.nguyencong92@gmail.com>
2017-08-13 13:04:23 +0700

    OnMouse Over

--- Assets/Scripts/StackController.cs
+++ Assets/Scripts/StackController.cs
@@ -6,4 +6,11 @@ public class StackController : MonoBehaviour {
 	public List<PlanetController> planets;
 
 
+   
+    // Select the stack by clicking on it
+    void OnMouseOver()
+    {
+        print("Name: " + gameObject.name);
+    }
+
 }
