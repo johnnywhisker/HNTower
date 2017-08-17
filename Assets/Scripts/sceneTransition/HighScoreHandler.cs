46298c41d61b39389ad345fa37ad540b4736ac44
AlecksJohannes <alecksjohanssen@gmail.com>
2017-08-17 10:38:28 +0700

    Add HighScore

---
+++ Assets/Scripts/sceneTransition/HighScoreHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreHandler : MonoBehaviour {
+	public void enteringHighScore() {
+		Application.LoadLevel ("HighScore");	
+	}
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
